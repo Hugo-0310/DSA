4ce8c4c15019a48d2e8d94a195d3f55ff1527872
hugo <hugo.isozaki0310@gmail.com>
2026-06-21 22:45:44 +1000

    Node class initial codes

--- Doubly_linked_list/Node.java
+++ Doubly_linked_list/Node.java
@@ -1,5 +1,11 @@
 package Doubly_linked_list;
 
 public class Node {
-    
+    int element;
+    Node prevNode;
+    Node nextNode;
+
+    public Node(int element){
+        this.element = element;
+    }
 }
