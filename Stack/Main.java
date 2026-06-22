646956b66935735df16735d1664104d5ff19ba73
hugo <hugo.isozaki0310@gmail.com>
2026-06-22 16:28:15 +1000

    stack v1 done

--- Stack/Main.java
+++ Stack/Main.java
@@ -1,5 +1,18 @@
 package Stack;
 
 public class Main {
-    
+    public static void main(String [] args){
+        Stack stack = new Stack(5);
+        stack.push(1);
+        stack.push(2);
+        stack.pop();
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        stack.capacity();
+        stack.size();
+        System.out.println(stack.traverse(0));
+    }
 }
--- Stack/Stack.java
+++ Stack/Stack.java
@@ -1,9 +1,56 @@
 package Stack;
 
 public class Stack {
+    int[] arr;
+    int n = 0;
+    int N;
     //create a stack object by specifing the number N for the size of an array. 
+    public Stack(int capacity){
+        N = capacity;
+        arr = new int[N];
+    }
+
     //push
+    public void push(int e){
+        if(n == N){
+            System.out.println("memory overflow error");
+        }else{
+            arr[n] = e;
+            n += 1;
+        }
+    }
     //pop
+    public void pop(){
+        if(n == 0){
+            System.out.println("no element in stack");
+        }else{
+            System.out.println("pop: "+arr[n-1]);
+            arr[n-1] = 0;
+            n -= 1;
+        }
+    }
     //size
+
+    public void size(){
+        System.out.println("size: "+n);
+    }
+
+    public void capacity(){
+        System.out.println("capacity: "+N);
+    }
+
     //traverse
+    public String traverse(int startingIndex){
+        //if n == 0, then dont return anything
+        if(n==0){
+            return null;
+        }else{
+            if(startingIndex == n-1){
+                return String.valueOf(arr[startingIndex]);
+            }else{
+                return arr[startingIndex] + "-" + traverse(startingIndex+1);
+            }
+        }
+        
+    }
 }
