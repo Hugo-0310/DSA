9df48e740c7e795b825f446422b3f5f3da29230b
hugo <hugo.isozaki0310@gmail.com>
2026-06-24 02:19:58 +1000

    SQ v1

--- Queue/SimipleQ/Main.java
+++ Queue/SimipleQ/Main.java
@@ -1,5 +1,24 @@
 package Queue.SimipleQ;
 
 public class Main {
-    
+    public static void main(String [] args){
+        SQ sq = new SQ(5);
+        sq.enqueue(1);
+        sq.enqueue(2);
+        sq.enqueue(3);
+        sq.enqueue(4);
+        sq.enqueue(5);
+        sq.enqueue(6);
+        System.out.println(sq.getRear());
+        sq.dequeue();
+        sq.dequeue();
+        sq.dequeue();
+        sq.dequeue();
+        sq.dequeue();
+        System.out.println(sq.getFront());
+        sq.dequeue();
+        System.out.println(sq.getSize());
+
+    }
+
 }
