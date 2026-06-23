d154d534c2d90e231a9a9d009e8bb984864c4c00
hugo <hugo.isozaki0310@gmail.com>
2026-06-24 02:42:53 +1000

    sll update

--- SinglyLinkedList/Main.java
+++ SinglyLinkedList/Main.java
@@ -6,7 +6,6 @@ public class Main {
         Node node2 = new Node(2);
         Node node3 = new Node(3);
         Node node4 = new Node(4);
-        Node node5 = new Node(5);
         SLL sll = new SLL(node2);
         sll.insertBefore(node2, node1);
         sll.insertAfter(node2, node3);
