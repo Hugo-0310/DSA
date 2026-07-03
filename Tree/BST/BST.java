c0520b69c9c046f3435c1a247c1285ede292616d
hugo <hugo.isozaki0310@gmail.com>
2026-07-03 16:02:37 +1000

    insertion almost done with while loop

--- Tree/BST/BST.java
+++ Tree/BST/BST.java
@@ -1,5 +1,31 @@
 package Tree.BST;
 
 public class BST {
+
+    Node root;
+    int size;
+
+    public BST(Node root){
+        this.root = root;
+    }
+
+    public void insert(Node node){
+        //no root
+        Node curr = this.root;
+        while(! isExternal(curr)){
+            if(node.element < curr.element){
+                curr = curr.left;
+            }else{
+                curr = curr.right;
+            }
+        }
+
+        //curr is the external node where the new node will be its child.
+        if(curr.element)
+    }
+
+    public boolean isExternal(Node node){
+        return (node.left == null) & (node.right == null);
+    }
     
 }
