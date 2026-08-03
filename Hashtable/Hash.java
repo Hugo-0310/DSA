f2317748b4270d142c9ee3d9a00b887745298324
hugo <hugo.isozaki0310@gmail.com>
2026-08-04 01:09:04 +1000

    hashtable with division hash function done no collision handling

--- Hashtable/Hash.java
+++ Hashtable/Hash.java
@@ -1,5 +1,31 @@
 package Hashtable;
 
 public class Hash {
-    
+    //public method that uses hash function to sort the initial array into hash table
+    public String[] sort(String[] arr){
+
+        String[] arr2 = new String[10];
+
+        for(String s : arr){
+            int index = hash(s);
+            arr2[index] = s;
+        }
+
+        return arr2;
+    }
+
+    //private method for hash function
+    private int hash(String s){
+        int x = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            int code = s.codePointAt(i);
+            x += code;
+        }
+
+        System.out.println(s+"'s hashcode is: "+x%10);
+        return x%10;
+    }
+
+
 }
--- Hashtable/Main.java
+++ Hashtable/Main.java
@@ -1,5 +1,15 @@
 package Hashtable;
+import java.util.Arrays;
 
 public class Main {
-    
+    public static void main(String [] args){
+
+        Hash hash = new Hash();
+        
+        String[] names = {"hugo", "leo", "max"};
+
+        String[] names2 = hash.sort(names);
+
+        System.out.println(Arrays.toString(names2));
+    }
 }
