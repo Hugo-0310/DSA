e41ee65cf81015529745e5c25724ed9116ab084c
hugo <hugo.isozaki0310@gmail.com>
2026-08-06 09:36:05 +1000

    separateChaining & lookup methods done

--- Hashtable/Hash.java
+++ Hashtable/Hash.java
@@ -1,18 +1,21 @@
 package Hashtable;
 
 public class Hash {
+
+    Node[] arr = new Node[10];
+   
     //public method that uses hash function to sort the initial array into hash table
-    public Node[] sort(Node[] arr){
+    // public Node[] sort(Node[] arr){
 
-        Node[] arr2 = new Node[10];
+    //     Node[] arr2 = new Node[10];
 
-        for(Node n : arr){
-            int index = hash(n.element);
-            arr2[index] = n;
-        }
+    //     for(Node n : arr){
+    //         int index = hash(n.element);
+    //         arr2[index] = n;
+    //     }
 
-        return arr2;
-    }
+    //     return arr2;
+    // }
 
     //private method for hash function
     private int hash(String s){
@@ -25,7 +28,16 @@ public class Hash {
         return x%10;
     }
 
-    public String contains(Node[] arr, String s){
+    public void insert(Node node){
+        int index = hash(node.element);
+        if(arr[index]==null){
+            arr[index] = node;
+        }else{
+            separateChain(arr[index], node);
+        }
+    }
+
+    public String contains(String s){
         int index = hash(s);
         if(arr[index].element == null){
             return "The hashtable does not contain the name: "+s;
@@ -33,13 +45,28 @@ public class Hash {
             if(arr[index].element.equals(s)){
                 return "The hashtable contains the name: "+s;
             }else{
+                //go thru the linked list here
+                Node curr = arr[index];
+                while(curr.next != null){
+                    if(curr.next.element.equals(s)){
+                        return "The hashtable contains the name: "+s;
+                    }
+                    curr = curr.next;
+                }
                 return "The hashtable does not contain the name: "+s;
             }
         }
     }
     
-    private void separateChain(){
+    private void separateChain(Node curr, Node newNode){
         //create a linked list from the cell
+        if(curr.next == null){
+            curr.next = newNode;
+        }else{
+            separateChain(curr.next, newNode);
+        }
     }
+
+
 
 }
--- Hashtable/Main.java
+++ Hashtable/Main.java
@@ -6,15 +6,19 @@ public class Main {
 
         Hash hash = new Hash();
         
-        Node node1 = new Node("Hugo");
-        Node node2 = new Node("Max");
+        Node node1 = new Node("AD");
+        Node node2 = new Node("BC");
         Node node3 = new Node("Leo");
+        Node node4 = new Node("max");
+        Node node5 = new Node("CB");
+    
+        hash.insert(node1);
+        hash.insert(node2);
+        hash.insert(node3);
+        hash.insert(node4);
+        hash.insert(node5);
 
-        Node[] names = {node1, node2, node3};
-        Node[] names2 = hash.sort(names);
-
-        System.out.println(Arrays.toString(names2));
-        System.out.println(hash.contains(names2, "Hugo"));
+        System.out.println(hash.contains("CB"));
     }
 }
 
